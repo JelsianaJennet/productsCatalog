06a5ff83cd3d995ddc9d7a80d19a54a1499ac0ce
Jelsiana Jennet <jelsianajennet@gmail.com>
2023-09-19 16:39:55 +0530

    UUID and DB COnnection

--- src/main/java/com/example/productcatalog/models/Product.java
+++ src/main/java/com/example/productcatalog/models/Product.java
@@ -1,10 +1,19 @@
 package com.example.productcatalog.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Product extends  BaseModel{
 
     private String title; //name
     private String description;
     private String image;
+    // P : C ->
+    // 1:1
+    // M:1
+    // M:1
+    @ManyToOne
     private Category category;
     private double price;
 
